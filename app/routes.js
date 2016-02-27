963667e5d73012bcb6583fb32874594b286076c2
mersocarlin <mersocarlin@gmail.com>
2016-02-27 11:15:10 -0300

    Remove unused route

--- app/routes.js
+++ app/routes.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './containers/app';
-import About from './containers/about';
 import Home from './containers/home';
 import Campaign from './containers/campaign';
-import Login from './containers/login'
+import Login from './containers/login';
 import NoMatch from './containers/no-match';
 
 import auth from '../services/auth';
 
 
 function requireAuth (nextState, replaceState) {
-  console.log('requireAuth');
   if (!auth.loggedIn()) {
     // replaceState({ nextPathname: nextState.location.pathname }, '/login');
   }
@@ -27,8 +25,7 @@ export default (
       <Route path="login" component={Login} />
       <Route path="home" component={Home} onEnter={requireAuth} />
       <Route path="campaign/:param1" component={Campaign} onEnter={requireAuth}/>
-      <Route path="about" component={About} />
-      <Route path="about/:param1/test/:param2" component={About} />
+
       <Route path="*" component={NoMatch}/>
     </Route>
   </Route>

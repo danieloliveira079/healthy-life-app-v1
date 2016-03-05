3a22ccd2bcb23d0740cef8fe3704b6c9a53603f4
mersocarlin <mersocarlin@gmail.com>
2016-03-05 11:30:47 -0300

    Add Dropbox api configuration

--- app/env/config.js
+++ app/env/config.js
@@ -2,4 +2,9 @@ export default {
   apiService: {
     url: process.env.API_SERVICE_URL,
   },
+  dropBoxApi: {
+    uploadPath: process.env.DROPBOX_UPLOAD_PATH,
+    uploadUrl: process.env.DROPBOX_UPLOAD_URL,
+    deleteUrl: process.env.DROPBOX_DELETE_URL,
+  },
 };
